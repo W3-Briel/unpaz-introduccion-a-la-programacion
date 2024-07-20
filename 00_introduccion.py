64afd8512d74a4aaaba77799760e922e4b74dae9
Angel G300 Saucedo <saucedog926@gmail.com>
2024-07-20 19:31:48 -0300

    hoja de ruta

---
+++ 00_introduccion.py
@@ -0,0 +1,7 @@
+# antes de introducirnos a la logica de los programas que vamos a relizar tendremos cuenta
+# como vamos a escribir estos mismos, a continuacion dejare un poco de sintaxis del lenguaje.
+
+
+## 1) variables: al crear una variable, estamos asignando implicitamente un espacio en memoria, en donde guardaremos datos.
+# las variables son cosas que simpre vamos a estar utilizando, asi que hay que darle bola en como se crean(declara) y se le otorga datos (cuando una variable ya tiene datos, se le dice que esta )
+#nombre = "datos"
