d03c510a32b7d534779e440492d11dbd38c0728e
Angel G300 Saucedo <saucedog926@gmail.com>
2024-07-24 22:57:33 -0300

    activity

--- 01_primeros_ejercicios.py
+++ 01_primeros_ejercicios.py
@@ -1,3 +1,7 @@
-##Escribir un programa que pregunte al usuario una cantidad a invertir, el interés anual y el
-##número de años, y muestre por pantalla el capital obtenido en la inversión. 
+##
 
+entrada_usuario = input("ingresar un valor: ")
+print("lo que ingreso el usuario es: "+ entrada_usuario)
+
+##Escribir un programa que pregunte al usuario una cantidad a invertir, el interés anual y el
+##número de años, y muestre por pantalla el capital obtenido en la inversión.
