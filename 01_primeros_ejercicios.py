0f20c9cf1add48254e1a69a9fa7910c188980b7b
Angel G300 Saucedo <saucedog926@gmail.com>
2024-07-25 22:17:29 -0300

    concatenacion y fstrings

--- 01_primeros_ejercicios.py
+++ 01_primeros_ejercicios.py
@@ -1,7 +1,8 @@
 ##
 
 entrada_usuario = input("ingresar un valor: ")
-print("lo que ingreso el usuario es: "+ entrada_usuario)
+print("1(esto es concatenacion de strings)lo que ingreso el usuario es: "+ entrada_usuario)
+print(f"2(esto fue hecho con un fstring)lo que ingreso el usuario es: {entrada_usuario}")
 
 ##Escribir un programa que pregunte al usuario una cantidad a invertir, el interés anual y el
 ##número de años, y muestre por pantalla el capital obtenido en la inversión.
