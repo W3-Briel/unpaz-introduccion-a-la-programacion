86f0a60ce0f12b00bfcddf36c8f8abc7ffd08ff8
Angel G300 Saucedo <saucedog926@gmail.com>
2024-08-02 21:28:02 -0300

    variables y tipos de datos

--- 00_introduccion.py
+++ 00_introduccion.py
@@ -6,10 +6,12 @@
 # las variables son cosas que simpre vamos a estar utilizando, asi que hay que darle bola en como se crean(declara) y se le otorga datos (cuando una variable ya tiene datos, se le dice que esta )
 #nombre = "datos"
 
+#nombre,apellido,edad = "angel","saucedo", 21
+
 ## 2) tipos de datos, los cuales son:
 # Strings, booleanos, listas, diccionarios, colecciones, floats, enteros
+strings,booleanos,listas,diccionarios,conjunto,flotante,entero = "hola",True, [],{"key":"value"},{1,2,3,4},12.3,12
 ## 2.2) operadores y operaciones con cada tipo de datos
-
 
 ## 3) estructuras de control
 # if else, elif, match case, operadores ternarios
