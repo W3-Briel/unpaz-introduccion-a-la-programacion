3c22fc2a4c70d6b081321763b313238499a1eaeb
Angel G300 Saucedo <saucedog926@gmail.com>
2024-08-13 23:39:38 -0300

    arreglando bug - ejercicio

--- 01_primeros_ejercicios.py
+++ 01_primeros_ejercicios.py
@@ -132,18 +132,18 @@
 #  */
 
 def diferentes_letras(str1: str, str2: str) -> tuple:
-    out1 = []
-    out2 = []
+    out1,out2 = [],[]
 
     for i in str1:
+        if i in out1: continue
         if i not in set(str2):
             out1.append(i)
     
     for x in str2:
-        if i not in set(str1):
+        if x in out2: continue
+        if x not in set(str1):
             out2.append(x)
     
     return (out1,out2)
 
-
-print(diferentes_letras("abc","asd"))
+print(diferentes_letras("abcasbasbsd1r","aasdffnseesd1"))
